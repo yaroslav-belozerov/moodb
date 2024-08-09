cf00979d24652598a7268b79031e45b48063438f
Yaroslav Belozerov <yaabelozerov@edu.hse.ru>
2024-08-09 21:12:45 +0300

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
